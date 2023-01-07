10ebe1cb10207eb19ce8404fd4bd15957f743a6f
menabaddo <edmundalexfresh@gmail.com>
2023-01-07 14:52:56 +0100

    I can perform basic calculations but I need to fix the clear button. It gives me an error once I clear to the last number.

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -15,6 +18,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     MaterialButton buttonDivide, buttonMultiply, buttonPlus, buttonMinus, buttonEquals;
     MaterialButton button0, button1, button2, button3, button4, button5, button6, button7, button8, button9;
     MaterialButton buttonAC, buttonDot;
+    String buttonText;
 
 
     @Override
@@ -53,6 +57,65 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
+        MaterialButton button = (MaterialButton) view; //whatever button is clicked
+
+        if (button.equals(buttonMultiply)) {
+           buttonText = "*";
+
+        }else {
+            buttonText = button.getText().toString();// it will get the text assigned initially to the button
+        }
+        String dataToCalculate = solutionTv.getText().toString();
+
+        if(buttonText.equals("AC")){
+            solutionTv.setText("");
+            resultTv.setText("0");
+            return;
+        }
+        if(buttonText.equals("=")){
+            solutionTv.setText(resultTv.getText());
+            return;
+        }
+        if(buttonText.equals("C")){
+            dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length()-1);
+        }
+
+
+        else{
+            dataToCalculate = dataToCalculate+buttonText;
+
+        }
+
+        solutionTv.setText(dataToCalculate);
+
+        String finalResult = getResult(dataToCalculate);
+
+
+        if(!finalResult.equals("Err")){
+            resultTv.setText(finalResult);
+        }
+
+
+
+
+
+    }
+
+    String getResult(String data) {
+        try {
+            Context context = Context.enter();
+            context.setOptimizationLevel(-1);
+            Scriptable scriptable = context.initStandardObjects();
+        String finalResult =    context.evaluateString(scriptable,data, "Javascript", 1, null).toString();
+            if (finalResult.endsWith(".0")) {
+                finalResult = finalResult.replace(".0", "");
+
+            }
+        return finalResult;
+        } catch (Exception e) {
+            return  "Err";
+
+        }
 
     }
 }
